1c433ba45ac419def0784bc69f657dcadc4ed2a9
Gabriel Gómez <gabotrugomez@2806-103e-0006-7cb6-1c9e-c1de-12dc-1000.ipv6.infinitum.net.mx>
2017-06-28 12:27:09 -0500

    Initial Commit

---
+++ WeatherApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  WeatherApp
+//
+//  Created by Gabriel Trujillo Gómez on 6/28/17.
+//  Copyright © 2017 Gabriel Trujillo Gómez. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
